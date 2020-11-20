dce8d8dd446b38b280e2ee476138f3b3d17e04b4
成葛格 <yang520464@126.com>
2020-11-20 09:55:58 +0800

    第一次 提交

---
+++ Week 07/stringnumber.js
@@ -0,0 +1,59 @@
+//完成 StringToNumber 和 NumberToString 两个函数
+// 进制 2、8、10、16
+function stringToNumber(str,radix){
+    const two_model = ['0', '1'];
+    const eight_model = ['0', '1', '2', '3', '4','5','6', '7'];
+    const ten_twomodel = ['0', '1', '2', '3', '4','5','6', '7','8','9'];
+    const sixteen_twomodel = ['0', '1', '2', '3', '4','5','6', '7','8','9','a','b','c','d','e','f'];
+
+    str = String(str).toLowerCase();
+    if(!radix){
+        radix = "10";
+    }
+    if("2" === radix || "8" === radix ||"10" === radix || "16" === radix){
+       if("2" === radix){
+            if(!str.startsWith("0b") || checkstr(str.subStr(2),two_model)){
+                return "格式不对！";
+            }
+        }
+        if("8" === radix){
+            if(!str.startsWith("0o") || checkstr(str.subStr(2),eight_model)){
+                return "格式不对！";
+            }
+        }
+        if("10" === radix){
+            if(!checkstr(str.subStr(2),two_model)){
+                return "格式不对！";
+            }
+        }
+        if("16" === radix){
+            if(!str.startsWith("0x") || checkstr(str.subStr(2),sixteen_twomodel)){
+                return "格式不对！";
+            }
+        }
+    }
+    
+    return  Number.parseInt(str,radix);
+}
+
+function checkstr(resouce,model) {
+    for(str of resouce){
+       if(model.indexOf(str) == -1){
+           return false;
+       }
+    }
+    return true;
+}
+
+function numberToString(num,radix){
+    try {
+        num = Number(num);
+    } catch (error) {
+        console.log(error);
+    }
+    return num.toString(radix);
+}
+
+
+
+//console.log(numberToString(0xf,"10"));
