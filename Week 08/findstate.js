27ceaa60319a562ba4022b5bbbc94b739ff056ad
成葛格 <yang520464@126.com>
2020-11-29 11:04:26 +0800

    使用状态机完成”abababx”的处理

--- Week 08/findstate.js
+++ Week 08/findstate.js
@@ -1,7 +1,64 @@
-function findstate(string) {
+// function findstate(string) {
+//     let state = start;
+//     for (const c of string) {
+//         state = state(c)
+//     }
+//     return state === end;
+// }
+
+// function start(str){
+//     if("a" === str)
+//         return findA;
+//     else    
+//         return start;
+// }
+
+// function findA(str){
+//     if("b" === str)
+//         return findB;
+//     else
+//         return start(str);
+// }
+
+// function findB(str){
+//     if("c" === str)
+//         return findC;
+//     else
+//         return start(str);
+// }
+
+// function findC(str){
+//     if("d" === str)
+//         return findD;
+//     else
+//         return start(str);
+// }
+
+// function findD(str){
+//     if("e" === str)
+//         return findE;
+//     else
+//         return start(str);
+// }
+
+// function findE(str){
+//     if("f" === str)
+//         return end;
+//     else
+//         return start(str);
+// }
+
+// function end(str){
+//    return end;
+// }
+
+// console.log(findstate("i ababcdefg"));
+//使用状态机完成”abababx”的处理。
+
+function match(string){
     let state = start;
     for (const c of string) {
-        state = state(c)
+        state = state(c);
     }
     return state === end;
 }
@@ -21,35 +78,42 @@ function findA(str){
 }
 
 function findB(str){
-    if("c" === str)
-        return findC;
+    if("a" === str)
+        return findA2;
     else
         return start(str);
 }
 
-function findC(str){
-    if("d" === str)
-        return findD;
+function findA2(str){
+    if("b" === str)
+        return findB2;
     else
         return start(str);
 }
 
-function findD(str){
-    if("e" === str)
-        return findE;
+function findB2(str){
+    if("a" === str)
+        return findA3;
     else
         return start(str);
 }
 
-function findE(str){
-    if("f" === str)
+function findA3(str){
+    if("b" === str)
+        return findB3;
+    else
+        return start(str);
+}
+
+function findB3(str){
+    if("x" === str)
         return end;
     else
-        return start(str);
+        return findB2(str);
 }
 
 function end(str){
-   return end;
-}
-
-console.log(findstate("i ababcdefg"));
+    return end;
+ }
+ 
+ console.log(match("abababx"));
